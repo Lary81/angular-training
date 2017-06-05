a114d218cb6dc52d17e385ac535c0597ec486561
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-06-05 13:05:14 +0200

    Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,6 +43,27 @@ export class AppComponent {
       category: '#00ff00',
       favourite: true
     },
+     {
+      id: 'book-1410',
+      title: 'Angular in action',
+      authors: 'Jan Kowalski',
+      category: '#00ff00',
+      favourite: true
+    },
+     {
+      id: 'book-1410',
+      title: 'Angular in action',
+      authors: 'Jan Kowalski',
+      category: '#00ff00',
+      favourite: true
+    },
+     {
+      id: 'book-1410',
+      title: 'Angular in action',
+      authors: 'Jan Kowalski',
+      category: '#00ff00',
+      favourite: true
+    },
     {
       id: 'book-1411',
       title: 'ReactJS in action',
