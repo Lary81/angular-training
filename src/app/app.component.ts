4fe757a50bd98df05c5f777a32e986d0757f6eb5
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-06-05 12:06:58 +0200

    Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
 
-  sayHello() {
-    this.title  = 'Hello!';
+   selected = {}
+   edited = null
+
+  titleSize = 24
+
+  get authorsStyle() {
+    return { color: 'gray', letterSpacing: '2px' }
   }
+
+  select(book) {
+    this.selected = book
+    this.edited = Object.assign({}, book)
+  }
+
+  save() {
+    Object.assign(this.selected, this.edited)
+    this.reset()
+  }
+
+  reset() {
+    this.selected = {}
+    this.edited = null
+  }
+
+  cancel() {
+    this.reset()
+  }
+
+  books = [
+    {
+      id: 'book-1410',
+      title: 'Angular in action',
+      authors: 'Jan Kowalski',
+      category: '#00ff00',
+      favourite: true
+    },
+    {
+      id: 'book-1411',
+      title: 'ReactJS in action',
+      authors: 'Jan Kowalski',
+      category: '#00ff00',
+      favourite: true
+    }
+  ]
 }
