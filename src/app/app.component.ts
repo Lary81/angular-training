e05f59a4297ef94291dce0e8a7ad81c743e6ddf7
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-06-05 10:35:27 +0200

    Base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'la-root',
+  selector: 'la-hello',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular';
+
+  sayHello() {
+    this.title  = 'Hello!';
+  }
 }
