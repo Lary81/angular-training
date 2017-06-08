e103b853b4ddc006883f4717795a8b9ab9673d2d
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-06-08 15:33:11 +0200

    awdwq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'la-hello',
+  selector: 'la-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
